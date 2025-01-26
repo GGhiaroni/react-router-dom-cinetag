f49fc1f1bdbb0c0f980d3af94547f154585c4eb6
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-26 14:02:27 -0300

    comecando a criar os cards de times

--- src/components/SectionEquipes/CardTime/index.jsx
+++ src/components/SectionEquipes/CardTime/index.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const CardTimeEstilizado = styled.div``;
+const CardEstilizado = styled.div`
+  background-color: #f4f4f4;
+  padding: 10px;
+  border-radius: 5px;
+  max-width: 200px;
+  img {
+    max-width: 130px;
+  }
+`;
+const CardTime = () => {
+  const [times, setTimes] = useState([]);
 
-const CardTime = () => {};
+  useEffect(() => {
+    fetch("/json/db.json")
+      .then((response) => response.json())
+      .then((data) => setTimes(data))
+      .catch((error) => console.error("Erro ao carregar o JSON:", error));
+  }, []);
+
+  return (
+    <CardEstilizado>
+      {times.map((time) => (
+        <div>
+          <img src={time.foto_escudo} alt={`escudo ${time.nome}`} />
+          <p key={time.id}>{time.nome}</p>
+        </div>
+      ))}
+    </CardEstilizado>
+  );
+};
 
 export default CardTime;
--- src/components/SectionEquipes/index.jsx
+++ src/components/SectionEquipes/index.jsx
@@ -1,5 +1,12 @@
-import TituloH1Estilizado from "../TituloH1Estilizado";
+import TituloH1Estilizado from "../TituloH1";
+import CardTime from "./CardTime";
+
 const SectionEquipes = () => {
-  return <TituloH1Estilizado>equipes em alta</TituloH1Estilizado>;
+  return (
+    <div>
+      <TituloH1Estilizado>equipes em alta</TituloH1Estilizado>
+      <CardTime />
+    </div>
+  );
 };
 export default SectionEquipes;
