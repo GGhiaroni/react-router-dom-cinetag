11ee15a3e152d659f3ba9533d9d428e165abdb3d
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-26 00:03:50 -0300

    refatorando layout footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,16 +4,26 @@ const FooterEstilizado = styled.footer`
   background-color: var(--preto);
   text-align: center;
   padding: 1rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   h2 {
     color: white;
     font-size: 16px;
+  }
+  img {
+    margin-left: 10px;
+    margin-right: 10px;
+    max-width: 25px;
   }
 `;
 
 const Footer = () => {
   return (
     <FooterEstilizado>
-      <h2>© Team Heritage 2025. Todos os direitos reservados.</h2>
+      <h2>©</h2>
+      <img src="/imagens/icone-leao-white.png" alt="icone leão branco" />
+      <h2>Team Heritage 2025. Todos os direitos reservados.</h2>
     </FooterEstilizado>
   );
 };
