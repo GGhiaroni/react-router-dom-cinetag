6867eae3e0abb878493e38757de41c60e4375cf6
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-28 13:53:47 -0300

    criando o contexto de favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FavoritosProvider from "./contextos/Favoritos";
 import Favoritos from "./pages/Favoritos";
 import Home from "./pages/Home";
 
@@ -8,10 +9,12 @@ function AppRoutes() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-      </Routes>
+      <FavoritosProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+        </Routes>
+      </FavoritosProvider>
       <Footer />
     </BrowserRouter>
   );
