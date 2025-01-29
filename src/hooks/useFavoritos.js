ffe5e699850282a7b96a85f9c920ef82dd9c0d7b
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-29 08:08:40 -0300

    criacao hook personalizado useFavoritos

---
+++ src/hooks/useFavoritos.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { FavoritosContext } from "../contextos/Favoritos";
+
+const useFavoritos = () => {
+  const { favorito, setFavorito } = useContext(FavoritosContext);
+
+  function adicionarFavoritos(novoFavorito) {
+    const favoritoRepetido = favorito.some(
+      (item) => item.id === novoFavorito.id
+    );
+
+    let novaListaFavoritos = [...favorito];
+
+    if (!favoritoRepetido) novaListaFavoritos.push(novoFavorito);
+    else {
+      novaListaFavoritos = novaListaFavoritos.filter(
+        (item) => item.id !== novoFavorito.id
+      );
+    }
+    setFavorito(novaListaFavoritos);
+  }
+  return { favorito, adicionarFavoritos };
+};
+
+export default useFavoritos;
