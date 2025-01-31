477edcc31e72c221d0b798bd68a41110a47dda43
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-31 19:38:44 -0300

    consertando o banner da pagina de time e colocando cores principais em todos os times do banco de dados

--- src/pages/PaginaDoTime.jsx
+++ src/pages/PaginaDoTime.jsx
@@ -9,7 +9,6 @@ const PaginaDoTimeEstilizado = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px;
   background-color: #f9f9f9;
   min-height: 100vh;
 `;
