75257733fafcc38f27dd4531a798ced2546d8055
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-31 11:29:04 -0300

    ajustando o layout da pagina principal

--- src/components/SectionEquipes/ListaTimes/index.jsx
+++ src/components/SectionEquipes/ListaTimes/index.jsx
@@ -1,24 +1,29 @@
 import styled from "styled-components";
 import listaTimes from "../../../../public/json/db.json";
-import CardTime from "../CardTime"; // Agora só importa o CardTime
+import TituloH1Estilizado from "../../TituloH1";
+import CardTime from "../CardTime";
 
 const CardEstilizadoContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
   justify-content: center;
-  padding: 20px 5px;
-  background-color: #f5f5f5;
-  border-radius: 10px;
+  padding: 20px;
+  border-radius: 20px;
+  width: 1200px;
+  margin: 20px auto;
 `;
 
 const ListaTimes = () => {
   return (
-    <CardEstilizadoContainer>
-      {listaTimes.map((time) => (
-        <CardTime key={time.id} time={time} />
-      ))}
-    </CardEstilizadoContainer>
+    <>
+      <TituloH1Estilizado>equipes em alta</TituloH1Estilizado>
+      <CardEstilizadoContainer>
+        {listaTimes.map((time) => (
+          <CardTime key={time.id} time={time} />
+        ))}
+      </CardEstilizadoContainer>
+    </>
   );
 };
 
